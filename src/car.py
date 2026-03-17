dda91b3a00153130fa0f879f840fd5a04ed75f6b
vspolohuika <decod264@gmail.com>
2026-03-17 12:27:23 +0300

    test1.2

--- src/car.py
+++ src/car.py
@@ -1,37 +1,22 @@
-# Собственные классы исключений.
-
-
-class TooMuchFuelError(Exception):
-    """Исключение при попытке залить слишком много бензина."""
-
-
-class NotEnoughFuelError(Exception):
-    """Исключение при недостатке бензина для поездки."""
-
-
-# Константы для сообщений об ошибках.
-TOO_MUCH_FUEL_MSG = "Вы пытаетесь залить слишком много бензина!"
-NOT_ENOUGH_FUEL_MSG = "Не доедем жеж..."
-
-
 class Car:
     def __init__(self, model: str, fuel_capacity: float) -> None:
-        self.model = model
-        self._fuel_in_tank: float = 0.0
+        self._model = model
         self._max_fuel_capacity: float = fuel_capacity
-
-    def refuel_car(self, fuel_quantity: float) -> None:
-        if self._max_fuel_capacity - self._fuel_in_tank < fuel_quantity:
-            raise TooMuchFuelError(TOO_MUCH_FUEL_MSG)
-        self._fuel_in_tank += fuel_quantity
-
-    def drive_car(self, distance_km: float) -> float:
-        # Считаем, что расход 8 литров на 100 км.
-        fuel_burned: int = 8 * (distance_km / 100)
-        if self._fuel_in_tank < fuel_burned:
-            raise NotEnoughFuelError(NOT_ENOUGH_FUEL_MSG)
-        self._fuel_in_tank -= fuel_burned
-        return self.get_current_fuel_level()
+        self._fuel_in_tank: float = 0
 
     def get_current_fuel_level(self) -> float:
         return self._fuel_in_tank
+
+    def refuel_car(self, fuel_quantity: float):
+        if self._max_fuel_capacity - self._fuel_in_tank < fuel_quantity:
+            raise Exception("Вы пытаетесь залить слишком много бензина!")
+        self._fuel_in_tank += fuel_quantity
+
+    def drive(self, distance_km: float):
+        # Считаем, что расход 8 литров на 100 км
+        fuel_burned: int = 8 * (distance_km / 100)
+        # TODO: Вася, не забудь расскомментировать! Клиенты могут застрять!!11
+        if self._fuel_in_tank < fuel_burned:
+            raise Exception("Не доедем жеж...")
+        self._fuel_in_tank -= fuel_burned
+        return self.get_current_fuel_level()
--- src/test_class_car.py
+++ src/test_class_car.py
@@ -1,5 +1,5 @@
 import unittest
-from .car import Car, TooMuchFuelError, NotEnoughFuelError
+from .car import Car
 
 class TestCase(unittest.TestCase):
     def setUp(self):
@@ -9,16 +9,12 @@ class TestCase(unittest.TestCase):
         pass
 
     def test_drive(self):
-        # Сначала нужно заправить машину
-        self.car.refuel_car(20)
-        # Проехать 20 км (сгорит 1.6 литра)
-        self.car.drive_car(20)
-        # Проверить, что нельзя проехать 80000 км без топлива
-        self.assertRaises(NotEnoughFuelError, lambda: self.car.drive_car(80000))
+        self.car.drive(20)
+        self.assertRaises(Exception, lambda: self.car.drive(80000))
 
     def test_refuel(self):
         # Заправим 20 литров
         self.car.refuel_car(20)
         assert self.car.get_current_fuel_level() == 20
         # Проверим, что будет исключение, если перельем
-        self.assertRaises(TooMuchFuelError, lambda: self.car.refuel_car(80))
+        self.assertRaises(Exception, lambda: self.car.refuel_car(80))
