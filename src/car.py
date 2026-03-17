1042fdb071db58d895edce2d156a40dc014de54b
vspolohuika <decod264@gmail.com>
2026-03-17 13:52:02 +0300

    test1.5.3

--- src/car.py
+++ src/car.py
@@ -1,4 +1,4 @@
-class Car_abcwreyn,m nbvcgmj,km nbvc vmnj:
+class Cardfcghoijpiuyfdyfgjkppiyfudtyrs:
     def __init__(self, model: str, fuel_capacity: float) -> None:
         self._model = model
         self._max_fuel_capacity: float = fuel_capacity
